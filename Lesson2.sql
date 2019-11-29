65fb05e00b6f88dd18889a93fa6dfbfff6e84b04
Rajanya Vazrala <vrajanya@gmail.com>
2019-11-29 18:16:16 -0500

    JOIN Examples

--- Lesson2.sql
+++ Lesson2.sql
@@ -34,7 +34,7 @@ JOIN accounts a ON s.id = a.sales_rep_id
 WHERE r.name = 'Midwest'
 ORDER BY AccountName;
 
---48 rows
+--48 rows in result
 
 -- Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a first name starting with S and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
 
